1acf1344d87f2409c326b24fb1e303ebb5e9c260
abilnbl17 <abilabil17@gmail.com>
2026-05-02 23:25:08 +0700

    Add: changes file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import { Button } from "@heroui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button>Testing Button </Button>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
