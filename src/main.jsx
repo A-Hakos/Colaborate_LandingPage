f67f9075a89290a20c16729611dffbe535bc1ead
abilnbl17 <abilabil17@gmail.com>
2026-05-03 15:59:44 +0700

    Update: Clean Code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
 import { HeroUIProvider } from "@heroui/system";
-// import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Pastikan ini ada
+import tailwindcss from "@tailwindcss/vite";
 
-// vite.config.js
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ["@heroui/react", "@heroui/system", "@heroui/theme"],
   },
 });
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     // tailwindcss(), // Tambahkan ini
-//   ],
-// });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(), // Plugin ini yang akan mencari file CSS dengan @import "tailwindcss"
-//   ],
-// });
