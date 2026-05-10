347faa1947f3afab8de74bdc79c134bcba739209
abilnbl17 <abilabil17@gmail.com>
2026-05-10 18:40:09 +0700

    Add: inprogress footer design UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import react from "react";
 export default function Footer() {
   return (
     <>
-      <footer className="w-full py-6 border-t border-divider bg-background">
+      <footer className="w-full py-6 border-t border-divider bg-green-900 text-white">
         <div className="container mx-auto px-6">
           <link
             rel="stylesheet"
@@ -20,34 +20,34 @@ export default function Footer() {
           <div className="flex flex-wrap justify-center items-top">
             <div className="flex flex-wrap text-left lg:text-left">
               <div className="w-full lg:w-6/12 px-4">
-                <h4 className="text-3xl fonat-semibold text-blueGray-700">
+                <h4 className="text-3xl fonat-semibold text-white">
                   Let's keep in touch!
                 </h4>
-                <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+                <h5 className="text-lg mt-0 mb-2 text-white">
                   Find us on any of these platforms, we respond 1-2 business
                   days.
                 </h5>
                 <div className="mt-6 lg:mb-0 mb-6">
                   <button
-                    className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                    className=" text-blue-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                     type="button"
                   >
                     <i className="fab fa-twitter"></i>
                   </button>
                   <button
-                    className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                    className=" text-blue-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                     type="button"
                   >
                     <i className="fab fa-facebook-square"></i>
                   </button>
                   <button
-                    className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                    className=" text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                     type="button"
                   >
                     <i className="fab fa-dribbble"></i>
                   </button>
                   <button
-                    className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                    className=" text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                     type="button"
                   >
                     <i className="fab fa-github"></i>
@@ -57,13 +57,13 @@ export default function Footer() {
               <div className="w-full lg:w-6/12 px-4">
                 <div className="flex flex-wrap items-top mb-6">
                   <div className="w-full lg:w-4/12 px-4 ml-auto">
-                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    <span className="block uppercase text-whitetext-sm font-semibold mb-2">
                       Useful Links
                     </span>
                     <ul className="list-unstyled">
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://www.creative-tim.com/presentation?ref=njs-profile"
                         >
                           About Us
@@ -71,7 +71,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://blog.creative-tim.com?ref=njs-profile"
                         >
                           Blog
@@ -79,7 +79,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://www.github.com/creativetimofficial?ref=njs-profile"
                         >
                           Github
@@ -87,7 +87,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile"
                         >
                           Free Products
@@ -96,13 +96,13 @@ export default function Footer() {
                     </ul>
                   </div>
                   <div className="w-full lg:w-4/12 px-4">
-                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    <span className="block uppercase text-white text-sm font-semibold mb-2">
                       Other Resources
                     </span>
                     <ul className="list-unstyled">
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
                         >
                           MIT License
@@ -110,7 +110,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://creative-tim.com/terms?ref=njs-profile"
                         >
                           Terms &amp; Conditions
@@ -118,7 +118,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://creative-tim.com/privacy?ref=njs-profile"
                         >
                           Privacy Policy
@@ -126,7 +126,7 @@ export default function Footer() {
                       </li>
                       <li>
                         <a
-                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          className="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                           href="https://creative-tim.com/contact-us?ref=njs-profile"
                         >
                           Contact Us
@@ -141,7 +141,7 @@ export default function Footer() {
           <div className="mt-6">
             <Separator className="my-4" />
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-blueGray-500 font-semibold py-1">
+              <div className="text-sm text-white font-semibold py-1">
                 Copyright © 2026{" "}
                 <a href="#" className="hover:text-gray-800">
                   Creative Tim
