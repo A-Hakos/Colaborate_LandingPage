6f433ae325131debf04d01a04cd8d4634b0b18d2
A-Hakos <adhithyaz12@gmail.com>
2026-05-10 18:53:58 +0700

    add carousel banner

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ function App() {
         <Banner />
 
         {/* Get Content Component */}
-        <main className="flex-grow p-4">
+        {/* <main className="grow p-4">
           <div className="p-8 bg-gray-100 rounded-lg shadow-md">
             <h1 className="text-3xl font-bold">Welcome to Our App</h1>
             <p>Content goes here...</p>
           </div>
-        </main>
+        </main> */}
 
         {/* Get Footer component */}
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
--- src/components/ui/carousel/Banner.jsx
+++ src/components/ui/carousel/Banner.jsx
@@ -1,37 +1,70 @@
 "use client"; //
 import React from "react";
 
+// Akan digunakan saat menggunakan swiper
+// Impor Komponen Utama
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Impor CSS Dasar Swiper
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// Impor Modul yang dibutuhkan (Opsional)
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
 export default function Banner() {
   return (
     <>
       {/* Banner */}
-      <div className="w-full h-[500px] relative">
+      {/* <div className="w-full h-125 relative">
         <img
           src="/banner.png"
           alt="Coffee"
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 bg-black/40"></div>
-        {/* Content */}
-        <div className="absolute inset-0 flex items-center justify-start">
-          {" "}
-          <div className="max-w-7xl ml-5 px-6 text-white">
-            <span className="bg-green-700/80 px-4 py-1 rounded-full text-sm">
-              Jelajahi rasa, suasana, dan cerita
-            </span>
-
-            <h1 className="text-4xl md:text-5xl font-bold mt-4 leading-tight">
-              Temukan Coffee Shop <br />
-              Terbaik di <span className="italic">Jawa Tengah</span>
-            </h1>
-
-            <p className="mt-4 text-gray-200 max-w-lg">
-              Dari hidden gems sampai cafe dengan view gunung, temukan tempat
-              ngopi favoritmu di Jawa Tengah.
-            </p>
-          </div>
+        <div className="absolute inset-0 bg-black/40"></div> */}
+      {/* Content */}
+      {/* <div className="absolute inset-0 flex items-center justify-start">
+          <div className="max-w-7xl ml-5 px-6 text-white"></div>
         </div>
-      </div>{" "}
+      </div> */}
+      <div className="w-full h-[400px] relative">
+        <div className="items-center justify-center">
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000 }}
+          >
+            <SwiperSlide>
+              <img
+                src="/Logo.png"
+                alt="Coffee"
+                className=" w-full h-[400px] object-cover"
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src="/banner2.png"
+                alt="Banner"
+                className=" w-full h-[400px] object-cover"
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src="/testimoni.png"
+                alt="Testi"
+                className=" w-full h-[400px] object-cover"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </>
   );
 }
