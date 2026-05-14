120784aacfa80acccab6af3440ddc5b1019736a3
A-Hakos <adhithyaz12@gmail.com>
2026-05-14 22:47:42 +0700

    Update : Clean code Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,63 +1,20 @@
 "use client"; //
 import React from "react";
-
-// Akan digunakan saat menggunakan swiper
-// Impor Komponen Utama
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Impor CSS Dasar Swiper
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-// Impor Modul yang dibutuhkan (Opsional)
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Carousel from "../ui/Carousel/Carousel";
 
 export default function Banner() {
+  // Data gambar didefinisikan di sini
+  const bannerData = [
+    { src: "/banner_awal.png", alt: "Banner Utama" },
+    { src: "/Banner_wil.png", alt: "Banner Wilayah" },
+    { src: "/banner_testi.png", alt: "Banner Testimoni" },
+  ];
+
   return (
-    <>
-      <div className="w-full relative overflow-hidden">
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          spaceBetween={0}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 6000 }}
-          speed={1000}
-          className="w-full"
-        >
-          <SwiperSlide>
-            <div className="w-full aspect-video md:h-[500px] lg:h-[70vh] relative">
-              <img
-                src="/banner_awal.png"
-                alt="Banner1"
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-          </SwiperSlide>
+    <section className="w-full">
+      {/* Panggil komponen Carousel dan kirimkan datanya melalui props 'items' */}
 
-          <SwiperSlide>
-            <div className="w-full aspect-video md:h-[500px] lg:h-[70vh] relative">
-              <img
-                src="/Banner_wil.png"
-                alt="Banner2"
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="w-full aspect-video md:h-[500px] lg:h-[70vh] relative">
-              <img
-                src="/banner_testi.png"
-                alt="Banner3"
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </>
+      <Carousel items={bannerData} />
+    </section>
   );
 }
